17b9c9553b911ee421697ac42a4abcd18c3c0557
Aluno <Aluno@DSN1251230723>
2022-04-01 14:54:55 -0300

    mensagem

---
+++ ExemploHeranca/src/Carro.java
@@ -0,0 +1,31 @@
+
+public class Carro extends Veiculo {
+ private boolean quatroPortas;
+
+ public boolean isQuatroPortas() {
+	 
+	 
+	 
+	 
+	 return quatroPortas;
+ }
+ 
+ 
+ public void setquatroPortas(boolean quatroPortas)  {
+	 
+	 this.quatroPortas = quatroPortas;
+	 
+ }
+
+
+@Override
+public void imprimeDados() {
+	// TODO Auto-generated method stub
+
+
+	super.imprimeDados();
+	System.out.println("Quatro Portas");
+}
+ 
+ 
+}
---
+++ ExemploHeranca/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+	
+Carro Caroo1 	= new Carro();		
+		
+	Caroo1.setAno(1900);	
+	Caroo1.marca = "Ford";
+			Caroo1.modelo = "Ford k";
+			Caroo1.setquatroPortas(true);
+	}
+
+}
